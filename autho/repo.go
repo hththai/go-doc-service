5d8de8809d68ddf6b56373c4e5ad1577381105b7
huythai <85726646+hththai@users.noreply.github.com>
2025-12-23 15:43:56 +1100

    Update repo.go

--- autho/repo.go
+++ autho/repo.go
@@ -2,6 +2,8 @@ package autho
 
 type AuthoRepo interface {
 	Register(user User) (User, error)
+	ChangePassword(user *User, newPassword string) (*User, error)
+	ValidatePassword(user *User, inputPassword string) (bool, error)
 }
 
 type AuthoRepoImpl struct {
@@ -14,4 +16,12 @@ func NewAuthoRepo() AuthoRepoImpl {
 
 func (r *AuthoRepoImpl) Register(user User) (User, error) {
 	return user, nil
+}
+
+func (r *AuthoRepoImpl) ChangePassword(user *User, newPassword string) (*User, error) {
+	return user, nil
+}
+
+func (r *AuthoRepoImpl) ValidatePassword(user *User, inputPassword string) (bool, error) {
+	return true, nil
 }
