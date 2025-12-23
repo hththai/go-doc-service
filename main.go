f1c1a9f86af74a912e16eec2771a8449d5a798d7
huythai <85726646+hththai@users.noreply.github.com>
2025-12-23 15:44:36 +1100

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"go_ocr/internal/autho"
+)
+
+type Result struct {
+	IsSuccess bool
+	Error     error
+}
+
+type Chain struct {
+	stringValue string
+	Result      Result
+}
+
+// Constructor and return Object.
+func NewChain() *Chain {
+	return &Chain{
+		Result: Result{
+			IsSuccess: false,
+			Error:     nil,
+		},
+	}
+}
+
+func (c *Result) SuccessResult() Result {
+	return Result{
+		IsSuccess: true,
+		Error:     nil,
+	}
+}
+
+func (c *Result) ErrorResult(err error) Result {
+	return Result{
+		IsSuccess: false,
+		Error:     err,
+	}
+}
+
+// Constructor.
+func (c *Chain) NewValue(s string) (*Chain, error) {
+
+	if len(s) > 5 {
+		c.Result = Result{
+			IsSuccess: false,
+			Error:     fmt.Errorf("len is too long"),
+		}
+
+		return c, c.Result.Error
+	}
+
+	c.Result.IsSuccess = true
+	return c, nil
+}
+
+func (c *Chain) Value() (*Chain, error) {
+	if c == nil || c.Result.IsSuccess != true {
+		return c, c.Result.Error
+	}
+	return c, nil
+}
+
+func (c *Chain) SetValue(s string) *Chain {
+
+	if len(s) > 5 {
+		c.Result = c.Result.ErrorResult(fmt.Errorf("input too long"))
+		return c
+	}
+
+	c.stringValue = s
+	c.Result = c.Result.SuccessResult()
+
+	return c
+}
+
 func main() {
 	authoRepo := autho.NewAuthoRepo()
 	authoService := autho.NewAuthoService(&authoRepo)
