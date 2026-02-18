da6d07c6da7be0394be680ceac04260e37a0e485
hththai <huythaicmvn@gmail.com>
2026-02-18 22:10:51 +1100

    refactor: remove not using client folders

--- client-ssr-sample/src/hooks/useRefresh.ts
+++
@@ -1,9 +0,0 @@
-// hooks/useRefresh.ts
-import { useMutation } from "@tanstack/react-query";
-import { refreshRequest } from "../api/auth";
-
-export function useRefresh() {
-    return useMutation({
-        mutationFn: refreshRequest,
-    });
-}
